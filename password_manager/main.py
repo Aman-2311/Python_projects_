870ae19951503a0d23483918b9b96c43300c699b
Aman07 <135688213+Aman-2311@users.noreply.github.com>
2026-03-15 17:01:51 +0530

    feat: add CLI menu for password manager

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,3 +1,27 @@
-from storage import load_passwords
-data = load_passwords()
-print(data)
+from manager import add_password,view_passwords,search_password,delete_password
+
+def menu():
+    while True:
+        print("1. Add new password")
+        print("2. View passwords")
+        print("3. Search passwords")
+        print("4. Delete passwords")
+        print("5. Exit")
+
+        choice =input("Enter your choice: ")
+
+        if choice =="1":
+            add_password()
+        elif choice =="2":
+            view_passwords()
+        elif choice =="3":
+            search_password()
+        elif choice =="4":
+            delete_password()
+        elif choice =="5":
+            exit()
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    menu()
