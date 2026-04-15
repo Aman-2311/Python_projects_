36aae6f98e4e8d28a04c899bb345178d80102cd3
Aman07 <135688213+Aman-2311@users.noreply.github.com>
2026-04-16 00:22:24 +0530

    feat: implement main CLI logic with argv parsing, input validation, and error handling

---
+++ weather_cli/weather.py
@@ -0,0 +1,25 @@
+#read cmd argument
+import sys
+from utils import get_weather
+
+#creat input function
+
+def main():
+    if len(sys.argv) < 2:
+        print("usage: python weather.py <city>")
+        return
+
+    city =sys.argv[1]
+    result =get_weather(city)
+
+    if 'error' in result:
+        print(result['error'])
+
+    else:
+        print(f"city:{result['city']}")
+        print(f"temp:{result['temp']}")
+        print(f"humidity:{result['humidity']}")
+        print(f"condition:{result['condition']}")
+
+if__name__ = "__main__"
+main()
